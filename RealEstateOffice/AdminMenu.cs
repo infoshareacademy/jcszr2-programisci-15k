90f6417cbe7e9e5b5311e487a617dc722ac24f92
Michal Nadstawny <michnadstawny@gmail.com>
2021-01-24 20:55:54 +0100

    added AdminMenu and StartMenu and specified user options in MainMenu

---
+++ RealEstateOffice/AdminMenu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstateOffice
+{
+    class AdminMenu
+    {
+        void AdminMainMenu()
+        {
+            //opcje zarządzania użytkownikami
+            //tylko ADMIN
+
+            //wyświetlanie wszystkich użytkowników
+            //edycja użytkowników
+            //dodawanie użytkowników
+            //usuwanie użytkowników
+
+            //opcja przeglądania logów systemowych
+            //tylko ADMIN
+
+            //opcja wylogowania się, powrót do StartMenu
+        }
+
+    }
+}
