f5ffd5fc299aac3ed51e1904328820d571cdc3fa
Małgorzata Nakonieczna <m.k.nakonieczna@gmail.com>
2021-01-26 21:35:11 +0100

    Szkielet menu

--- RealEstateOffice/FilterMenu.cs
+++ RealEstateOffice/FilterMenu.cs
@@ -1,4 +1,6 @@
-﻿namespace RealEstateOffice
+﻿using System;
+
+namespace RealEstateOffice
 {
     class FilterMenu
     {
@@ -8,6 +10,17 @@
             //Pytania po jakich kryteriach filtrować wpisy które mają zostać wyświetlone
             //(TypeOfRealEstate; Price; Area; OwnerName; OwnerSurname; City; Street; EstateAddress; CreationDate; ModificationDate;)
             //kiedy z backendu dojdzie lista obiektów RealEstate ma wywołać się Display() z RealEstatesView żeby wyświetlić listę nieruchomości
+
+            //Console.WriteLine("Po której kolumnie ma zostać przefiltrowana lista?");
+            //Console.WriteLine("1. Typ nieruchomości");
+            //Console.WriteLine("2. Cena");
+            //Console.WriteLine("3. Powierzchnia");
+            //Console.WriteLine("4. Imię właściela");
+            //Console.WriteLine("5. Nazwisko właściciela");
+            //Console.WriteLine("6. Miasto");
+            //Console.WriteLine("7. Ulica");
+            //Console.WriteLine("8. Numer posesji");
+            //Console.ReadLine();
         }
     }
 }
