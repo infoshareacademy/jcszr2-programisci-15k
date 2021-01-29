8355a424b0dcf0dec83444c76bbe24a6c4977b77
Michal Nadstawny <michnadstawny@gmail.com>
2021-01-29 13:22:25 +0100

    made almost functional FilterOperationMenu and finished Filter class fixed missing bracket in AddMenu added fields to RealEstate

--- RealEstateOffice/Filter.cs
+++ RealEstateOffice/Filter.cs
@@ -1,16 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Resources;
 using System.Text;
+using Microsoft.VisualBasic;
 
 namespace RealEstateOffice
 {
     class Filter
     {
+        public Filter()
+        {
+            _typeOfRealEstate = null;
+            OwnerName = null;
+            OwnerSurname = null;
+            City = null;
+            Street = null;
+            PriceLowest = null;
+            PriceHighest = null;
+            AreaSmallest = null;
+            AreaBiggest = null;
+            CreationDateEarliest = null;
+            CreationDateLatest = null;
+            ModificationDateEarliest = null;
+            ModificationDateLatest = null;
+        }
 
-        //obiekt tej klasy jest tworzony przez pytanie użytkownika o to które wpisy wyświetlić w FilterOperationMenu()
-        //po czym jest przekazywany do backendu gdzie te filtry są już użyte
-        //posiada pola: (TypeOfRealEstate; PriceLowest; PriceHighest;
-        //AreaLowest; AreaHighest; OwnerName; OwnerSurname; City; Street;
-        //CreationDateEarliest; CreationDateLatest; ModificationDateEarliest; ModificationDateLatest;)
+        public void FilterReset()
+        {
+            _typeOfRealEstate = null;
+            OwnerName = null;
+            OwnerSurname = null;
+            City = null;
+            Street = null;
+            PriceLowest = null;
+            PriceHighest = null;
+            AreaSmallest = null;
+            AreaBiggest = null;
+            CreationDateEarliest = null;
+            CreationDateLatest = null;
+            ModificationDateEarliest = null;
+            ModificationDateLatest = null;
+        }
+
+        public void ShowActiveFilters()
+        {
+            if(_typeOfRealEstate != null) {
+                Console.WriteLine($"Kategoria nieruchomości: {_typeOfRealEstate}");
+            }
+            if (OwnerName != null && OwnerSurname != null)
+            {
+                Console.WriteLine($"Właściciel: {OwnerName} {OwnerSurname}");
+            }
+            if (City != null)
+            {
+                Console.WriteLine($"Miejscowość: {City}");
+            }
+            if (Street != null)
+            {
+                Console.WriteLine($"Ulica: {Street}");
+            }
+            if (PriceLowest != null && PriceHighest != null)
+            {
+                Console.WriteLine($"Cena: od {PriceLowest} PLN do {PriceHighest} PLN");
+            }
+            if (AreaSmallest != null && AreaBiggest != null)
+            {
+                Console.WriteLine($"Powierzchnia: od {AreaSmallest} m^2 do {AreaBiggest} m^2");
+            }
+            if (CreationDateEarliest != null && CreationDateLatest != null)
+            {
+                Console.WriteLine("Wpis nieruchomości dodany między {0:dd/MM/yyyy} a {1:dd/MM/yyyy}", CreationDateEarliest, CreationDateLatest);
+            }
+            if (ModificationDateEarliest != null && ModificationDateLatest != null)
+            {
+                Console.WriteLine("Wpis nieruchomości zaktualizowany między {0:dd/MM/yyyy} a {1:dd/MM/yyyy}", ModificationDateEarliest, ModificationDateLatest);
+            }
+        }
+
+        private RealEstate.TypeOfRealEstate ? _typeOfRealEstate;
+        public string OwnerName;
+        public string OwnerSurname;
+        public string City;
+        public string Street;
+        public int ? PriceLowest;
+        public int ? PriceHighest;
+        public int ? AreaSmallest;
+        public int ? AreaBiggest;
+        public DateTime ? CreationDateEarliest;
+        public DateTime ? CreationDateLatest;
+        public DateTime ? ModificationDateEarliest;
+        public DateTime ? ModificationDateLatest;
+        public RealEstate.TypeOfRealEstate ? TypeOfRealEstate
+        {
+            set
+            {
+                _typeOfRealEstate = value;
+            }
+        }
     }
 }
--- RealEstateOffice/RealEstate.cs
+++ RealEstateOffice/RealEstate.cs
@@ -1,11 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace RealEstateOffice
 {
     class RealEstate
     {
+        public RealEstate()
+        {
+            _isIdSet = false;
+            _isCreationDateSet = false;
+        }
+        
+        private bool _isIdSet;
+        private bool _isCreationDateSet;
+        
         //pola obiektu: ID; TypeOfRealEstate; Price; Area; OwnerName; OwnerSurname; City; Street; EstateAddress; CreationDate; ModificationDate;
+
+
+        private int _id;
+        public enum TypeOfRealEstate
+        {
+            Dom,    // 0
+            Mieszkanie,   // 1
+            Działka,      // 2
+            Garaż,      // 3
+            Lokal_usługowy // 4
+        }
+
+        private TypeOfRealEstate _typeOfRealEstate;
+        private int _price;
+        private int _area;
+        private string _ownerName;
+        private string _ownerSurname;
+        private string _city;
+        private string _street;
+        private string _estateAddress;
+        private DateTime _creationDate;
+        private DateTime _modificationDate;
+
+        public int Id
+        {
+            set
+            {
+                if (_isIdSet == true)
+                {
+                    throw new InvalidOperationException("id can only be set once");
+                }
+                _isIdSet = true;
+                _id = value;
+            }
+        }
+        public DateTime CreationDate
+        {
+            set
+            {
+                if (_isCreationDateSet == true)
+                {
+                    throw new InvalidOperationException("creation date can only be set once");
+                }
+                _isCreationDateSet = true;
+                _creationDate = value;
+            }
+        }
+
+
     }
 }
