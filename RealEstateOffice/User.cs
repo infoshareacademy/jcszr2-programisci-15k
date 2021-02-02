bf440301987a9526a6a1e149ac1d0ca698789865
marcincsharp <marcin.kurscsharp@gmail.com>
2021-02-02 22:02:14 +0100

    UserDatabaseContext.AddUser implementation

--- RealEstateOffice/User.cs
+++ RealEstateOffice/User.cs
@@ -6,9 +6,32 @@ namespace RealEstateOffice
 {
     class User
     {
-        //pole enum userType które wyznacza uprawnienia (admin = 1, pracownik biura nieruchomości = 2, klient = 3)
+       //odpowiednik jednego wiersza w Users.csv
+       //posiada pola ID;UserType; Login; Password; Name; Surname; EmailAddress;
+       //pole enum userType które wyznacza uprawnienia (admin = 0, pracownik biura nieruchomości = 1, klient = 2
+       private int _id;
+       private UserType _userType;
+       private string _login;
+       private string _password;
+       private string _name;
+       private string _surname;
+       private string _emailAddress;
+
+        public int Id { get => _id; set => _id = value; }
+        public UserType UserType { get => _userType; set => _userType = value; }
+        public string Login { get => _login; set => _login = value; }
+        public string Password { get => _password; set => _password = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Surname { get => _surname; set => _surname = value; }
+        public string EmailAddress { get => _emailAddress; set => _emailAddress = value; }
         
-        //odpowiednik jednego wiersza w Users.csv
-        //posiada pola ID;UserType; Login; Password; Name; Surname; EmailAddress;
+    }
+
+    public enum UserType
+    {
+        Admin,    // 0
+        Employee,   // 1
+        Client,      // 2
+       
     }
 }
--- RealEstateOffice/UserDatabaseContext.cs
+++ RealEstateOffice/UserDatabaseContext.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace RealEstateOffice
@@ -8,12 +10,55 @@ namespace RealEstateOffice
     {
         //metoda  która  pobierze  wszystkich uzytkowników
         //ListOfUsers();
-        //metoda do dodawania uzytkowników
-        //AddUser()
         //metoda do uswania 
         //RemoveUser();
         //metoda do edycji uzytkowników
         //EditUser();
+
+        //metoda do dodawania uzytkowników
+        public static void AddUser(User user)
+        {
+            //ID; Login; Password; Name; Surname; EmailAddress; UserType;
+            String path = "..\\Files\\Users.csv";
+            string realativePath = DatabaseContext.bingPathToAppDir(path);
+
+            var lastLine = File.ReadLines(realativePath).Last(); // z tego wyciągam  ID 
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("");
+            sb.Append(lastLine); //Id
+            sb.Append(";");
+            sb.Append(user.Login);
+            sb.Append(";");
+            sb.Append(user.Password);
+            sb.Append(";");
+            sb.Append(user.Name);
+            sb.Append(";");
+            sb.Append(user.Surname);
+            sb.Append(";");
+            sb.Append((int)user.UserType);
+     
+            using (StreamWriter sw = File.AppendText(realativePath))
+            {
+                sw.Write(sb);
+
+            }
+
+            Console.Clear();
+            Console.WriteLine("User record added to database.Press any key");
+            Console.ReadLine();
+
+        }
+
+
+        public static string bingPathToAppDir(string localPath)
+        {
+            string currentDir = Environment.CurrentDirectory;
+            DirectoryInfo directory = new DirectoryInfo(
+            Path.GetFullPath(Path.Combine(currentDir, @"..\..\" + localPath)));
+            return directory.ToString();
+        }
+
 
 
     }
