8702fdc7c515e7578cafefb27ec0afd361596c18
Małgorzata Nakonieczna <m.k.nakonieczna@gmail.com>
2021-02-07 20:04:55 +0100

    Remove user menu added

---
+++ RealEstateOffice/RemoveUserMenu.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstateOffice
+{
+    class RemoveUserMenu
+    { //pytamy o ID użytkownika do usunięcia i przekazujemy do UserDatabaseContext do metody RemoveUser
+    }
+}
