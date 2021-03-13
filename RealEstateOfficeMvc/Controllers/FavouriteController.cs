11d3f0415ea8d54107dd5b91cc83ca264a3e457c
marcincsharp <marcin.kurscsharp@gmail.com>
2021-03-13 20:21:55 +0100

    Likes backend, new buttons

---
+++ RealEstateOfficeMvc/Controllers/FavouriteController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealEstateOfficeMvc.Controllers
+{
+    public class FavouriteController : Controller
+    {
+
+        public void GetUserFavourites()
+        {
+            //input : userId 
+            //pobieramy rekordy polubien z  Favourites.csv
+            //output : lista obiektów typu Favourite 
+            
+        }
+
+        public void GetFavouriteRealEstates()
+        {
+            //input : lista obiektów typu Favourite 
+            //output :  listę już konkretnych nieruchomosci
+        }
+
+       
+        public IActionResult Index()
+        {
+            //input : lista obiektów typu realEstate
+            //return view
+            return View();
+        }
+
+        public  void  Like()
+        {
+            //tu tworzymy wpisy do pliku favourites.csv
+        }
+
+        public void Unlike()
+        {
+            //tutaj usuwamy wpisy z pliku favourites 
+        }
+
+    }
+}
